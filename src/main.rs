79d79ebcce762f9a44aa07dcbadde06dd5e5c4fc
Saurabh <saurabhm@proton.me>
2022-11-14 16:44:02 -0800

    removed unused endpoint

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,8 @@ async fn greet(req: HttpRequest) -> impl Responder {
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+    HttpServer::new(|| App::new().route("/hello", web::get().to(greet)))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
