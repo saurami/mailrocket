66fb6e755748f0852c8f016da7b38d03e7b1ae00
Saurabh Mishra <saurabhm@proton.me>
2022-11-16 17:43:13 -0800

    common code shared with integration test files

---
+++ tests/common/mod.rs
@@ -0,0 +1,9 @@
+use std::net::TcpListener;
+
+pub fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port ...");
+    let port = listener.local_addr().unwrap().port();
+    let server = mailrocket::run(listener).expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
+}
