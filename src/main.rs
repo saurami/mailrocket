6ea336edd519244505202c28ad5e46825a505125
Saurabh Mishra <saurabhm@proton.me>
2022-11-16 17:41:20 -0800

    run app on random port

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use mailrocket::run;
+use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    run(TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port"))?.await
 }
