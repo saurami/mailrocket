e33edbff3cdb5adf36ed088c14756ba85470d04c
Saurabh Mishra <saurabhm@proton.me>
2022-11-16 19:51:26 -0800

    initiate logger to trace level by default

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+use env_logger::Env;
 use mailrocket::run;
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("trace")).init();
     run(TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port"))?.await
 }
